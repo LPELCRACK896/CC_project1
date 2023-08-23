55a72076fea5fde40d2080f2864bc0b1031d5b96
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-08-23 16:32:49 -0600

    initial commit, from lab1

---
+++ builder.py
@@ -0,0 +1,156 @@
+import os
+import subprocess
+from tkinter import filedialog, Tk
+
+
+def generate_main_file(directory, start_rule="expr"):
+    grammar_name = directory.rsplit("/", 1)[1]
+    with open(f"{directory}/main.py", "w", encoding="utf-8") as f:
+        f.write("import os\n")
+        f.write("from tkinter import filedialog, Tk\n")  # Added this line
+        f.write("from anytree.exporter import DotExporter\n")
+        f.write("from antlr4 import *\n")
+        f.write(f"from {grammar_name}Lexer import {grammar_name}Lexer\n")
+        f.write(f"from {grammar_name}Parser import {grammar_name}Parser\n")
+        f.write("from anytree import Node, RenderTree\n")
+        f.write("from anytree.exporter import UniqueDotExporter\n")
+        f.write("import antlr4\n")
+        f.write("import re\n")
+        f.write("from antlr4.tree.Tree import TerminalNode\n\n")
+
+        f.write('''
+class CustomErrorListener(antlr4.error.ErrorListener.ErrorListener):
+    def __init__(self):
+        self.errors = []
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        # error en español
+        translations = {
+            r"missing (.+) at (.+)": r"falta \\1 en \\2",
+            r"mismatched input (.+) expecting (.+)": r"entrada no coincidente \\1, se esperaba \\2",
+            r"extraneous input '(.+)' expecting (.+)": r"entrada innecesaria '\\1', se esperaba \\2",
+            # Agrega más traducciones aquí si lo deseas
+        }
+
+        # Buscar coincidencias con las expresiones regulares y traducir el mensaje
+        for pattern, translation in translations.items():
+            msg = re.sub(pattern, translation, msg)
+
+        # Personalizar el mensaje de error aquí
+        custom_msg = f"Error en la línea {line}, columna {column}, mensaje: {msg}"
+        self.errors.append(custom_msg)
+
+    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
+        pass
+
+    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
+        pass
+
+    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
+        pass
+                
+    def get_errors(self):
+        return self.errors\n\n''')
+
+        # Below code to get file as input
+        f.write("root = Tk()\n")
+        f.write("root.withdraw()\n")
+        f.write("input_file = filedialog.askopenfilename(initialdir=os.getcwd(),filetypes=(('YAPL files', '*.yapl'),('All files', '*.*')))\n")
+        f.write("with open(input_file, 'r') as file:\n")
+        f.write("    input_data = file.read()\n")
+        f.write("input_stream = InputStream(input_data)\n")
+
+        f.write("error_listener = CustomErrorListener()\n")
+
+        f.write(f"lexer = {grammar_name}Lexer(input_stream)\n")
+        f.write(f"lexer.removeErrorListeners()\n")
+        f.write(f"lexer.addErrorListener(error_listener)\n\n")
+
+        f.write("stream = CommonTokenStream(lexer)\n")
+
+        f.write(f"parser = {grammar_name}Parser(stream)\n")
+
+        f.write(f"parser.removeErrorListeners()\n")
+        f.write(f"parser.addErrorListener(error_listener)\n\n")
+
+        f.write("# Aplica la regla inicial de la gramática (expr)\n")
+        f.write(
+            f"tree = parser.{start_rule}()# Linea a cambiar en funcion de la regla inicial del parser\n\n")
+
+        f.write('''
+# tdos los errores se imprimen si hay
+errors = error_listener.get_errors()
+for error in errors:
+    print(error)
+
+if errors:
+    print("----------------------------------------------------------------------------------")
+    print("\\nYa que hay 1 o más errores no se armará el árbol sintáctico del archivo input.\\n")
+    print("----------------------------------------------------------------------------------")
+else:
+    print(tree.toStringTree(recog=parser))
+
+    def build_anytree(node, antlr_node):
+        if isinstance(antlr_node, TerminalNode):
+            value = antlr_node.getText()
+            # Replace double quotes with single quotes
+            value = value.replace('"', "'")
+            Node(value, parent=node)
+        else:
+            rule_name = parser.ruleNames[antlr_node.getRuleIndex()]
+            child_node = Node(rule_name, parent=node)
+            for child in antlr_node.getChildren():
+                build_anytree(child_node, child)
+
+    root = Node(parser.ruleNames[tree.getRuleIndex()])
+    build_anytree(root, tree)
+
+    # Imprime el árbol anytree
+    for pre, fill, node in RenderTree(root):
+        print(f'{pre}{node.name}')
+
+    # Genera una representación visual del árbol anytree
+    dot_exporter = UniqueDotExporter(root)
+    dot_exporter.to_picture("visual_tree.png")
+    os.system(f"start visual_tree.png")
+''')
+
+
+def list_g4_files():
+    return [f for f in os.listdir() if f.endswith('.g4')]
+
+
+def generate_antlr_files(start_rule="expr"):
+    root = Tk()
+    root.withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    file = filedialog.askopenfilename(initialdir=os.getcwd(), filetypes=(
+        ("ANTLR4 files", "*.g4"), ("All files", "*.*")))  # open the dialog GUI
+    if file:
+        directory = file.rsplit(".", maxsplit=1)[0]
+        cmd = f'java -jar antlr-4.13.0-complete.jar -Dlanguage=Python3 -o "{directory}" "{file}"'
+        try:
+            if not os.path.exists(directory):
+                os.makedirs(directory)
+            process = subprocess.run(
+                cmd, shell=True, check=True, stdout=subprocess.PIPE, universal_newlines=True)
+            output = process.stdout
+            print(output)
+
+            # Generate main.py file for the grammar
+            generate_main_file(directory=directory, start_rule=start_rule)
+
+        except subprocess.CalledProcessError as e:
+            print(
+                f"Error generating ANTLR files for {file}. Command '{cmd}' returned non-zero exit status {e.returncode}.")
+
+
+def compile_YAPL(start_rule="program"):
+    generate_antlr_files(start_rule=start_rule)
+
+
+def main():
+    compile_YAPL()
+
+
+if __name__ == "__main__":
+    main()
---
+++ project1/main_YAPL.py
@@ -0,0 +1,128 @@
+import os
+from tkinter import filedialog, Tk
+from antlr4 import *
+from YAPLLexer import YAPLLexer
+from YAPLParser import YAPLParser
+from anytree import Node, RenderTree
+from anytree.exporter import UniqueDotExporter
+import antlr4
+import re
+from antlr4.tree.Tree import TerminalNode
+from ClassSymbolTable import SymbolTable, Scope  # Make sure to import the SymbolTable and Scope classes
+
+
+def build_anytree(node, antlr_node):
+    if isinstance(antlr_node, TerminalNode):
+        value = antlr_node.getText()
+        # Replace double quotes with single quotes
+        value = value.replace('"', "'")
+        Node(value, parent=node)
+    else:
+        rule_name = parser.ruleNames[antlr_node.getRuleIndex()]
+        child_node = Node(rule_name, parent=node)
+        for child in antlr_node.getChildren():
+            build_anytree(child_node, child)
+
+# Define a custom error listener
+class CustomErrorListener(antlr4.error.ErrorListener.ErrorListener):
+    def __init__(self):
+        self.errors = []
+
+    def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
+        # error en español
+        translations = {
+            r"missing (.+) at (.+)": r"falta \1 en \2",
+            r"mismatched input (.+) expecting (.+)": r"entrada no coincidente \1, se esperaba \2",
+            r"extraneous input '(.+)' expecting (.+)": r"entrada innecesaria '\1', se esperaba \2",
+            # Agrega más traducciones aquí si lo deseas
+        }
+
+        # Buscar coincidencias con las expresiones regulares y traducir el mensaje
+        for pattern, translation in translations.items():
+            msg = re.sub(pattern, translation, msg)
+
+        # Personalizar el mensaje de error aquí
+        custom_msg = f"Error en la línea {line}, columna {column}, mensaje: {msg}"
+        self.errors.append(custom_msg)
+
+    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
+        pass
+
+    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
+        pass
+
+    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
+        pass
+
+    def get_errors(self):
+        return self.errors
+    
+root = Tk()
+root.withdraw()
+input_file = filedialog.askopenfilename(initialdir=os.getcwd(
+), filetypes=(('YAPL files', '*.yapl'), ('All files', '*.*')))
+with open(input_file, 'r') as file:
+    input_data = file.read()
+input_stream = InputStream(input_data)
+error_listener = CustomErrorListener()
+lexer = YAPLLexer(input_stream)
+lexer.removeErrorListeners()
+lexer.addErrorListener(error_listener)
+
+stream = CommonTokenStream(lexer)
+parser = YAPLParser(stream)
+parser.removeErrorListeners()
+parser.addErrorListener(error_listener)
+
+# Aplica la regla inicial de la gramática (expr)
+tree = parser.program()
+
+# Print errors if any
+errors = error_listener.get_errors()
+for error in errors:
+    print(error)
+
+if errors:
+    print("----------------------------------------------------------------------------------")
+    print("\nYa que hay 1 o más errores no se armará el árbol sintáctico del archivo input.\n")
+    print("----------------------------------------------------------------------------------")
+else:
+    print(tree.toStringTree(recog=parser))
+
+
+
+    root = Node(parser.ruleNames[tree.getRuleIndex()])
+    build_anytree(root, tree)
+
+    # Imprime el árbol anytree
+    for pre, fill, node in RenderTree(root):
+        print(f'{pre}{node.name}')
+
+    # Genera una representación visual del árbol anytree
+    dot_exporter = UniqueDotExporter(root)
+    dot_exporter.to_picture("visual_tree.png")
+    os.system(f"start visual_tree.png")
+
+    # Build the symbol table
+    symbol_table = SymbolTable(root)
+
+    print(symbol_table)
+
+    # Inserta símbolo en global scope
+    symbol_table.insert(name="MySymbol", data_type="int", semantic_type="var", value=5, scope=symbol_table.global_scope)
+
+
+    print(symbol_table)
+
+    searched_symbol = symbol_table.search("MySymbol")
+    print("\nBúsqueda de símbolo en tabla:")
+    print(searched_symbol)
+
+    symbol_table.delete_content("MySymbol")
+
+    searched_symbol = symbol_table.search("MySymbol")
+    print("\Símbolo tras eliminacion:")
+    print(searched_symbol)
+
+    print(symbol_table)
+
