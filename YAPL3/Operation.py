a6ec3a7a7a8999540d0a6ea139f5e0d59785f8f5
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-10-10 20:52:39 -0600

    Update Operation.py

--- YAPL3/Operation.py
+++ YAPL3/Operation.py
@@ -3,20 +3,16 @@ from typing import Dict, AnyStr
 
 class Operation:
     type_of_operation: str
-    translation_operation_to_str: Dict[AnyStr, AnyStr]
 
     def __init__(self, type_of_operation: AnyStr | None):
         self.type_of_operation = type_of_operation
-        self.translation_operation_to_str = \
-            {
-                "assign": "ASS",
-            }
+
 
     def __str__(self):
         if self.type_of_operation is None:
             return ""
 
         if len(self.type_of_operation) > 5:
-            return str(self.type_of_operation[:5]).upper()
+            return str(self.type_of_operation[:6]).upper()
 
         return f"{self.type_of_operation}"
