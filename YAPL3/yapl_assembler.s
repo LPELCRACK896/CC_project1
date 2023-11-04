60425e881cecaf8f56416a1e500ef810828bd4a6
MarianoReyes <josemarianoreyesh02@gmail.com>
2023-11-04 15:38:44 -0600

    pruebas en inputs

--- YAPL3/yapl_assembler.s
+++ YAPL3/yapl_assembler.s
@@ -1,13 +1,16 @@
-li $t1, 10
-li $t0, 4
-add 	t1, $t1, $t0
-li $t1, 5
-li $t0, 2
-div $t1, $t0
-mflo 	t2
-li $t1, 10
+li $t1, 4
+li $t0, 3
+mul 	t1, $t1, $t0
 li $t0, 10
-sub 	t3, $t1, $t0
+li $t1, t1
+add 	t2, $t0, $t1
+li $t0, 5
+li $t1, 2
+div $t0, $t1
+mflo 	t3
+li $t0, 10
 li $t1, 10
-li $t0, 7
-mul 	t4, $t1, $t0
+sub 	t4, $t0, $t1
+li $t0, 10
+li $t1, 7
+mul 	t5, $t0, $t1
