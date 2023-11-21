f1732ae1bf9b2991540a7cbcf3708e98c46a4cdd
Luis Pedro Gonzalez Aldana <60456157+LPELCRACK896@users.noreply.github.com>
2023-11-20 18:06:45 -0600

    simple input

--- YAPL3/output.asm
+++ YAPL3/output.asm
@@ -5,10 +5,10 @@
 	buffer: .space 1024
 	Main_attr_booleano: .byte 1
 	Main_attr_age: .word 0
-	Main_attr_name: .asciiz " "
-	temporal_var_1: .asciiz " ingrese su edad: "
-	temporal_var_2: .asciiz "\nEs mayor de edad"
-	temporal_var_3: .asciiz "\nEs menor de edad"
+	temporal_var_1: .asciiz "Hola Carlos ingrese su edad: "
+	temporal_var_2: .asciiz "\n"
+	temporal_var_3: .asciiz "\nEs mayor de edad"
+	temporal_var_4: .asciiz "\nEs menor de edad"
 .text
 
 	j Main_main
@@ -36,22 +36,6 @@
 		syscall
 		jr $ra
 	Main_main:
-		la $t0, Main_attr_name
-		li $t1, 76
-		sb $t1, 0($t0)
-		li $t1, 117
-		sb $t1, 1($t0)
-		li $t1, 105
-		sb $t1, 2($t0)
-		li $t1, 115
-		sb $t1, 3($t0)
-		li $t1, 0
-		sb $t1, 5($t0)
-		la $t0, Main_attr_name
-		addi $sp, $sp -4
-		sw $t0, 0($sp)
-		jal IO_outString
-		addi $sp, $sp, 4
 		la $t0, temporal_var_1
 		addi $sp, $sp -4
 		sw $t0, 0($sp)
@@ -62,11 +46,21 @@
 		move $t0, $v0
 		la $t1, Main_attr_age
 		sw $t0, 0($t1)
+		la $t0, temporal_var_2
+		addi $sp, $sp -4
+		sw $t0, 0($sp)
+		jal IO_outString
+		addi $sp, $sp, 4
+		lw $t0, Main_attr_age
+		addi $sp, $sp -4
+		sw $t0, 0($sp)
+		jal IO_outInt
+		addi $sp, $sp, 4
 		lw $t0, Main_attr_age
 		li $t1, 17
 		slt $t2, $t1, $t0
 		beq $t2, $zero, L2
-		la $t2, temporal_var_2
+		la $t2, temporal_var_3
 		addi $sp, $sp -4
 		sw $t2, 0($sp)
 		jal IO_outString
@@ -75,7 +69,7 @@
 
 
 		L2:
-		la $t2, temporal_var_3
+		la $t2, temporal_var_4
 		addi $sp, $sp -4
 		sw $t2, 0($sp)
 		jal IO_outString
